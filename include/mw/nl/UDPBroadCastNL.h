13e45478b952d3006f144de91c4380cfe6e4f857
mschulze <mschulze@235a714a-1e17-0410-b3bb-947d69a96898>
2008-05-15 07:34:18 +0000

    adding a stub for an  UDP BroadCast Network Layer
    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@340 235a714a-1e17-0410-b3bb-947d69a96898

---
+++ include/mw/nl/UDPBroadCastNL.h
@@ -0,0 +1,61 @@
+#ifndef __UDPBroadCastNL_h__
+#define __UDPBroadCastNL_h__
+
+#include "mw/nl/Packet.h"
+#include "mw/common/NodeID.h"
+#include "mw/common/Subject.h"
+#include "mw/api/EventChannel.h"
+#include "debug.h"
+#include <stdio.h>
+
+class UDPBroadCastNL {
+public:
+    struct info{
+	enum {
+	    payload=8
+	};
+    };
+
+    typedef int32_t SNN;
+
+    typedef Packet<SNN> Packet_t;
+
+
+    UDPBroadCastNL() { init();}
+    ~UDPBroadCastNL() {}
+
+    void init(const NodeID &i) {
+	DEBUG(("%s Configuration 64Bit NodeID=%lld\n", __PRETTY_FUNCTION__, i.value));
+    }
+
+    // bind Subject to specific networks name
+    void bind(const Subject &s, SNN &snn) {
+	DEBUG(("%s\n", __PRETTY_FUNCTION__));
+    }
+
+    void deliver(const Packet_t& p) {
+	DEBUG(("%s\n", __PRETTY_FUNCTION__));
+    }
+
+    void deliver_fragment(const Packet_t& p) {
+	DEBUG(("%s\n", __PRETTY_FUNCTION__));
+    }
+
+    void fetch(Packet_t& p) {
+	DEBUG(("%s\n", __PRETTY_FUNCTION__));
+    }
+
+    void interrupt() {
+	DEBUG(("%s\n", __PRETTY_FUNCTION__));
+    }
+
+};
+
+#endif /* __UDPBroadCastNL_h__ */
+
+/* This stuff is for emacs
+ * Local variables:
+ * mode:c++
+ * c-basic-offset: 4
+ * End:
+ */
