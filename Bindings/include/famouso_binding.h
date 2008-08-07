71161f19dc7cfed76d8108f2130733c60aa6896d
mschulze <mschulze@235a714a-1e17-0410-b3bb-947d69a96898>
2008-08-07 11:47:23 +0000

    needed includes for Bindungs
    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@418 235a714a-1e17-0410-b3bb-947d69a96898

--- Bindings/include/famouso_binding.h
+++ Bindings/include/famouso_binding.h
@@ -1,15 +1,22 @@
-#ifndef _famouso_bindings_h_
-#define _famouso_bindings_h_
+#ifndef _FAMOUSO_PY_H_
+#define _FAMOUSO_PY_H_
 
-#include "mw/el/EventLayerClientStub.h" 
-#include "mw/api/EventChannel.h"
-#include "mw/api/PublisherEventChannel.h"
-#include "mw/api/SubscriberEventChannel.h"
+#include <sys/types.h>
 
-#include "mw/common/Event.h"
+#define MAX_EVENT_DATA_LENGTH	8
 
-typedef EventChannel < EventLayerClientStub > EC;
-typedef PublisherEventChannel  < EC > PEC;
-typedef SubscriberEventChannel < EC > SEC;
+typedef unsigned long long famouso_subject_t;
+
+typedef struct {
+	famouso_subject_t subject;
+	int len;
+	char data[MAX_EVENT_DATA_LENGTH];
+} famouso_event_t; 
+
+int famouso_announce(famouso_subject_t subject);
+int famouso_subscribe(famouso_subject_t subject);
+int famouso_unsubscribe(famouso_subject_t subject);
+int famouso_publish(famouso_event_t event);
+int famouso_poll(famouso_event_t *event);
 
 #endif
