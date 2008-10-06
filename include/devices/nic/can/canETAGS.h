7a6964e1cb6e026034d3e33610c7bb17f98f8532
mschulze <mschulze@235a714a-1e17-0410-b3bb-947d69a96898>
2008-10-06 14:17:10 +0000

    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@558 235a714a-1e17-0410-b3bb-947d69a96898

---
+++ include/devices/nic/can/canETAGS.h
@@ -0,0 +1,24 @@
+#ifndef __canETAGS_h__
+#define __canETAGS_h__
+
+namespace famouso {
+  namespace mw {
+    namespace nl {
+      namespace CAN {
+	
+	/*! \brief Predefined etags for ccp and etag exchange */
+	namespace ETAGS {
+	  enum {
+	    CCP_SSI		= 0x0,
+	    CCP_RSI,
+	    GET_ETAG,
+	    SUPPLY_ETAG
+	  };
+
+	} /* namespace ETAGS */
+      } /* namespace CAN */
+    } /* namespace nl */
+  } /* namespace mw */
+} /* namespace famouso */
+
+#endif
