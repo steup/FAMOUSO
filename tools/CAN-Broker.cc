ee42f0a808a4eb8cf0e26521071cbbd6ec7b8be3
mschulze <mschulze@235a714a-1e17-0410-b3bb-947d69a96898>
2008-10-06 14:16:05 +0000

    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@556 235a714a-1e17-0410-b3bb-947d69a96898

--- tools/CAN-Broker.cc
+++ tools/CAN-Broker.cc
@@ -1,94 +1,15 @@
+
 #include "devices/nic/can/peak/PeakCAN.h"
+#include "devices/nic/can/canETAGS.h"
+#include "devices/nic/can/canID.h"
 #include "mw/common/UID.h"
 
 char *dev = "/dev/pcan32";
 
 typedef device::nic::CAN::PeakCAN<dev, 0x011c> can;
+typedef famouso::mw::nl::CAN::detail::ID ID;
 typedef can::MOB	mob;
 can canDriver;
-
-void callback() {
-    std::cout << "rx_Interrupt from CAN -- Callback " << std::endl;
-}
-
-namespace famouso {
-    namespace mw {
-        namespace nl {
-            namespace CAN {
-                namespace ETAGS {
-                    enum {
-                        CCP_SSI		= 0x0,
-                        CCP_RSI,
-                        GET_ETAG,
-                        SUPPLY_ETAG
-                    };
-                }
-                union ID {
-                    uint32_t value;
-                    class __attribute__((packed)) {
-                        uint16_t _etag      :
-                            14;
-                        uint8_t  _tx_nodelo :
-                            2;
-                        uint8_t  _tx_nodehi :
-                            5;
-                        int8_t   _priolo    :
-                            3;
-                        int8_t   _priohi    :
-                            5;
-                        uint8_t             :
-                            3;
-                        public:
-                            uint16_t etag() {
-                                return _etag;
-                            }
-                            void etag(uint16_t e) {
-                                _etag = e;
-                            }
-                            uint8_t tx_node() {
-                                return _tx_nodelo | (_tx_nodehi << 2) ;
-                            }
-                            void tx_node(uint8_t t) {
-                                _tx_nodelo = t & 0x3;
-                                _tx_nodehi = t >> 2 ;
-                            }
-                            uint8_t prio() {
-                                return _priolo | (_priohi << 3) ;
-                            }
-                            void prio(uint8_t p) {
-                                _priolo = p & 0x7;
-                                _priohi = p >> 3;
-                            }
-                    } parts;
-
-                    class __attribute__((packed)) {
-                        uint16_t _etag    :
-                            14;
-                        uint8_t  _nibblelo :
-                            2;
-                        uint8_t  _nibblehi :
-                            2;
-                        uint8_t  _stage  :
-                            4;
-//                        uint16_t        : 9;
-                        public:
-                            uint16_t etag() {
-                                return _etag;
-                            }
-                            uint8_t stage() {
-                                return _stage;
-                            }
-                            uint8_t nibble() {
-                                return _nibblelo | (_nibblehi << 2) ;
-                            }
-                    } parts_ccp;
-                    uint8_t v[4];
-                }__attribute__((packed));
-
-            }
-        }
-    }
-};
 
 //
 // damit der Algorithmus ordentlich funktioniert, ist eine
@@ -130,22 +51,26 @@ class CCP_Broker {
         }
 
         bool handle_ccp_rsi(CAN_MOB &mob) {
+//           if ( ccp_stage == 15 )
+//                std::cout << "CAN Configuration Protocol request for nodeID" << std::hex << std::endl;
 
-            famouso::mw::nl::CAN::ID *id = reinterpret_cast<famouso::mw::nl::CAN::ID*>(&mob.ID);
-
+			ID *id = reinterpret_cast<ID*>(&mob.ID);
             // extrahiere zunaechst die wichtigsten Infos
-            uint8_t nibble = id->parts_ccp.nibble();
-            uint8_t stage = id->parts_ccp.stage();
+            uint8_t nibble = id->ccp_nibble();
+            uint8_t stage = id->ccp_stage();
             uint8_t tx_node = constants::Broker_tx_node;
 
-            std::cout	<< std::hex << "etag="	<< id->parts_ccp.etag()
-            << "\tnibble=" << (uint32_t)id->parts_ccp.nibble()
-            << "\tstage=" << (uint32_t)id->parts_ccp.stage() << std::endl;
+//            std::cout << std::hex << "etag="	<< id->etag()
+//                      << "\tnibble=" << static_cast<uint32_t>(nibble)
+//                      << "\tstage=" <<  static_cast<uint32_t>(ccp_stage) << std::endl;
 
             // kommt die Nachricht von jemandem der im richtigen stage ist?
             if (!(ccp_stage == stage)) {
-                std::cerr << "received ccp rsi for stage " << (uint32_t)stage << " but I am in stage "
-                << (uint32_t)ccp_stage << " dropping it." << std::endl;
+                std::cerr << "received ccp rsi for stage "
+                          << static_cast<uint32_t>(stage)
+                          << " but I am in stage "
+                          << static_cast<uint32_t>(ccp_stage)
+                          << " dropping it." << std::endl;
                 return false;
             } else {
 
@@ -162,9 +87,9 @@ class CCP_Broker {
 
                 // 0xFD is non-real-time
                 // \todo RealTimeClasses for the CAN-Bus needs to be defined somewhere
-                id->parts.prio(0xFD);
+                id->prio(0xFD);
 
-                id->parts.etag(famouso::mw::nl::CAN::ETAGS::CCP_SSI);
+                id->etag(famouso::mw::nl::CAN::ETAGS::CCP_SSI);
 
                 mob.LEN = 8;
 
@@ -180,49 +105,108 @@ class CCP_Broker {
                     // der knoten keine tx_node bekommt
                     if ( (tx_node = search_tx_node(uid)) > constants::Broker_tx_node) {
                         std::cerr << "more than " << constants::Broker_tx_node
-                        << " nodes configured that means something went wrong" << std::endl;
+                                  << " nodes configured that means something went wrong" << std::endl;
                         return false;
                     } else {
-                        std::cout << std::hex << "UID 0x" << uid.value
-                        << " [" << (char*)&uid.value << "] "
-                        << "got short node id tx_node="
-                        << (uint32_t)tx_node << std::endl;
+                        std::cout << std::hex << "CCP\t\t -- UID 0x" << uid.value
+                                  << " [" << reinterpret_cast<char*>(&uid.value) << "] "
+                                  << "-> tx_node [0x"
+                                  << static_cast<uint32_t>(tx_node) << "]" << std::endl;
                     }
                     ccp_stage = 15;
                     uid = 0;
                 }
                 // tx_node ist entweder die des Brokers, wenn noch nicht stage 0 und
                 // sonst die txnode des neuen Knoten
-                id->parts.tx_node(tx_node);
+                id->tx_node(tx_node);
             }
             return true;
         }
 };
 
-CCP_Broker<mob> broker;
+// old ETAG supply protocol
+template < class CAN_MOB>
+class ETAG_Broker {
+        struct constants {
+            enum {
+				reserved = 100,
+                Broker_tx_node = 0x7f,
+                count = (1 << 14)-reserved
+            };
+        };
+
+        UID etags[constants::count];
+
+    public:
+        ETAG_Broker() {
+            for ( uint16_t i = 0; i < constants::count; ++i ) {
+                etags[i].value = 0;
+            }
+        }
+
+        bool get_etag(CAN_MOB &mob) {
+			ID *id = reinterpret_cast<ID*>(&mob.ID);
+			UID uid(*reinterpret_cast<UID*>(mob.DATA));
+
+			uint16_t etag=0;
+			while (etag < constants::count) {
+                if ( (etags[etag] == 0 ) || ( etags[etag] == uid ) )
+					break;
+				++etag;
+			}
+            if ( etag ==  constants::count) {
+				std::cout << "no free etags -- that should never be happen" << std::endl;
+				return false;
+			} else {
+				etags[etag]=uid;
+				etag+=constants::reserved;
+				mob.LEN=4;
+				mob.DATA[0] = id->tx_node();
+				mob.DATA[1] = 0x3;
+				mob.DATA[2] = etag >> 8;
+				mob.DATA[3] = static_cast<uint8_t>(etag & 0xff);
+                id->etag(famouso::mw::nl::CAN::ETAGS::SUPPLY_ETAG);
+				id->tx_node(constants::Broker_tx_node);
+				std::cout << "Supply etag\t -- UID [0x" << std::hex << uid.value << "]"
+						  << " -> etag [0x" << etag << "]" << std::endl;
+				return true;
+			}
+         }
+};
+
 
 int main(int argc, char **argv) {
-    //dev=argv[1];
-    mob m;
+    std::cout << "Project: FAMOUSO" << std::endl;
+    std::cout << "CAN Broker providing CAN Configuration Protocol and Binding Protocol" << std::endl << std::endl;
+    std::cout << "Author: Michael Schulze" << std::endl;
+    std::cout << "Revision: $Rev$" << std::endl;
+    std::cout << "$Date$" << std::endl;
+    std::cout << "last changed by $Author$" << std::endl << std::endl;
+    std::cout << "build Time: " << __TIME__ << std::endl;
+	std::cout << "build Date: " << __DATE__ << std::endl << std::endl;
+
+	// allow /dev/pcanXX as parameter for PeakCAN driver
+	if (argc > 1)
+		dev=argv[1];
+
+	CCP_Broker<mob> ccp_broker;
+	ETAG_Broker<mob> etag_broker;
+
     canDriver.init();
-    //canDriver.set_rx_Interrupt(&callback);
-    std::cout << "CAN Broker providing CAN Configuration Protocol and Binding Protocol" << std::endl;
     while (1) {
+		mob m;
         canDriver.receive_blocking(&m);
-        famouso::mw::nl::CAN::ID *id = reinterpret_cast<famouso::mw::nl::CAN::ID*>(&m.ID);
-        switch ( id->parts.etag()) {
+        ID *id = reinterpret_cast<ID*>(&m.ID);
+        switch ( id->etag()) {
             case famouso::mw::nl::CAN::ETAGS::CCP_RSI: {
-//            std::cout << "CAN Configuration Protocol request for nodeID" << std::hex << std::endl;
-//            std::cout << "ID=" << id->value
-//            << "\tetag=" << id->parts.etag()
-//            << "\ttx_node=" << (uint32_t)id->parts.tx_node()
-//            << "\tprio=" << (uint32_t)id->parts.prio() << std::endl;
-                    if ( broker.handle_ccp_rsi(m) )
+                    if ( ccp_broker.handle_ccp_rsi(m) )
                         canDriver.send(m);
                     break;
                 }
             case famouso::mw::nl::CAN::ETAGS::GET_ETAG: {
-                    std::cout << "got ETAG_Request" << std::endl;
+                    if ( etag_broker.get_etag(m) )
+                        canDriver.send(m);
+                    break;
                 }
 
         }
