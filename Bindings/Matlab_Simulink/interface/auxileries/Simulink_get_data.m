8937f633f3da8869f50ce237a562e84a28f9f5af
zug <zug@235a714a-1e17-0410-b3bb-947d69a96898>
2008-10-09 13:52:13 +0000

    - bug fix
    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@583 235a714a-1e17-0410-b3bb-947d69a96898

--- Bindings/Matlab_Simulink/interface/auxileries/Simulink_get_data.m
+++ Bindings/Matlab_Simulink/interface/auxileries/Simulink_get_data.m
@@ -2,14 +2,28 @@ function value=Simulink_get_data(subject, method)
 	channel_name=id2name('input',subject);
     eval(sprintf('global %s;',channel_name));
     eval(sprintf('[data %s] =get_data(%s, method);',channel_name,channel_name));
-    value=[0 0 0 0 0 0 0 0];
-    if ~isempty(data)
-        aux = max(size(data));
-        if aux>8 
-            aux=8;
-        end
-        for i=1:aux
-            value(i)=data(i);
-        end
+    
+    
+    switch method
+        case 'newest'
+            if ~isempty(data)
+                value=[0 0 0 0 0 0 0 0];
+                aux = max(size(data));
+                if aux>8 
+                    aux=8;
+                end
+                for i=1:aux
+                    value(i)=data(i);
+                end
+            end
+           value=double(value);
+        case 'all'
+            % hier wird jetzt fuer das Szenario geschummelt und der
+            % erstbeste Datansatz mit der richtigen Robot ID gesucht
+            data=double(data);
+            a=find(data(:,1)==1);
+            value=[0 0 0];
+            if ~isempty(a)
+                value=data(a(1),:);
+            end
     end
-    value=double(value);
