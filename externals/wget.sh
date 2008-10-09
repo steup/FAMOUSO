e00ce9f168c1896cad8279633242d7e4014df487
mschulze <mschulze@235a714a-1e17-0410-b3bb-947d69a96898>
2008-10-09 16:56:21 +0000

    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@595 235a714a-1e17-0410-b3bb-947d69a96898

--- externals/wget.sh
+++ externals/wget.sh
@@ -1,6 +1,6 @@
 BOOST_VERSION=boost_1_36_0
 rm -rf Boost boost*
 
-wget http://mesh.dl.sourceforge.net/sourceforge/boost/${BOOST_VERSION}.tar.bz2
+wget http://prdownloads.sourceforge.net/sourceforge/boost/${BOOST_VERSION}.tar.bz2
 tar xfj ${BOOST_VERSION}.tar.bz2
 mv ${BOOST_VERSION} Boost
