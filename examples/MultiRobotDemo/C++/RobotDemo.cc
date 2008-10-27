6eead91c21e244d2a71aa78dace39fa1a14af752
mschulze <mschulze@235a714a-1e17-0410-b3bb-947d69a96898>
2008-10-27 14:31:38 +0000

    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@648 235a714a-1e17-0410-b3bb-947d69a96898

---
+++ examples/MultiRobotDemo/C++/RobotDemo.cc
@@ -0,0 +1,85 @@
+/*
+ * RobotDemo.cc
+ *
+ *
+ *  Michael Schulze, 2008
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+
+#include "famouso_bindings.h"
+#include "util/Thread.h"
+#include "util/endianess.h"
+
+#define RobotID 3
+
+int done;
+char *HumanDetectionSubject="HumanDet";
+char *Distance="Distance";
+char *Velocity="Velocity";
+
+volatile uint8_t DistanceSensorValue=200;
+
+void SensorValueCB(famouso::mw::api::SECCallBackData& cbd) {
+    if ((cbd.data[2] == RobotID)){
+        DistanceSensorValue=cbd.data[0];
+    }
+}
+
+volatile uint8_t Human=0;
+void HumanDetectedCB(famouso::mw::api::SECCallBackData& cbd) {
+    Human=cbd.data[0];
+};
+
+void siginthandler(int egal) {
+    done = 1;
+}
+
+int main(int argc, char **argv) {
+    done = 0;
+    signal(SIGINT,siginthandler);
+
+    printf("FAMOUSO -- Initialization started.\n");
+    printf("FAMOUSO -- Initialization successful.\n");
+
+    SEC sec(htonll(*(uint64_t*)Distance));
+    sec.subscribe();
+    sec.callback.bind<SensorValueCB>();
+
+    SEC H(htonll(*(uint64_t*)HumanDetectionSubject));
+    H.subscribe();
+    H.callback.bind<HumanDetectedCB>();
+
+    PEC pec(htonll(*(uint64_t*)Velocity));
+    famouso::mw::Event e(pec.subject());
+	uint8_t data[3]={120,120,RobotID};
+	e.length = 3;
+	e.data = data;
+    pec.announce();
+
+    while(!done) {
+        if (Human!=0) {
+            // drive(STOP);
+            data[0]=0;
+            data[1]=0;
+        } else {
+            if ( DistanceSensorValue < 50) {
+                // drive(LEFT);
+                data[0]=20;
+                data[1]=-20;
+            } else {
+                // drive(STRAIGHT);
+                data[0]=120;
+                data[1]=120;
+            }
+        }
+        pec.publish(e);
+        // mam muss nicht unbedingt warten, jedoch entlasstet dies
+        // die cpu, weil es sonst busy-waiting ist
+        usleep(50000);
+    }
+}
---
+++ examples/MultiRobotDemo/Python/DistanceVisualisation.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+# make Famouso Python Bindings Package available in the path
+# for the package loader
+import sys
+sys.path.append('../../../Bindings/Python/')
+
+# load the Famouso Python Bindings
+from pyFAMOUSO.famouso import *
+
+# standard bibliothek
+import signal, struct
+from threading import Thread
+
+import math
+from random import randint
+import pygame
+
+from array import array
+
+class DistanceVisualiser:
+    def __init__(self):
+        # set basic parameters such as width and height of the screen and so on
+        self.width=400
+        self.height=200
+        self.act_y=self.height / 2
+        self.max=100
+
+        # init pygame and setup screen
+        pygame.init()
+        pygame.display.init()
+        pygame.display.set_mode((self.width,self.height),pygame.DOUBLEBUF|pygame.HWSURFACE)
+
+        # make black surface for clearing the screen... faster than fill?
+        self.screen = pygame.display.get_surface()
+        self.black = pygame.Surface((self.width,self.height))
+        # initialize the balls-array
+        self.distance_array = []
+
+        # calculate inital stars
+        for i in range(self.max):
+            self.distance_array.append(60)
+
+        self.clock = pygame.time.Clock()
+
+    def update(self, value):
+        self.screen.fill((0,0,0))
+        del self.distance_array[0]
+        self.distance_array.append(value)
+        # Starupdate
+        for dis in xrange(1,self.max-1):
+            pygame.draw.line(self.screen,(125,255,155),(dis*4,self.height-self.distance_array[dis]*2),(4*(dis+1),self.height-self.distance_array[dis+1]*2))
+        pygame.display.flip()
+
+
+
+## Globale Variablen
+RobotID=4
+
+## Asyncore Loop Class
+
+class LoopThread(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+    def run(self):
+        try:
+            print "asyncore.loop beendet"
+        except:
+            print "asyncore.loop wurde extern beendet"
+
+
+## Callback Klasse
+
+class CallBack:
+    def LogCallback(self, myEvent):
+        e=struct.unpack("BcB",myEvent.content)
+        if e[2] == RobotID:
+            if e[0] < 60:
+                displayDistance.update(e[0])
+            else:
+                displayDistance.update(60)
+
+
+## Singal Handler
+
+def sig_handler(signum, frame):
+        LogSubscriber.unsubscribe()
+        print "Alle Sockets geschlossen, mit ENTER beenden"
+        sys.exit(1)
+
+
+## MAIN
+if len(sys.argv)>1:
+    RobotID=int(sys.argv[1])
+signal.signal(signal.SIGINT, sig_handler)
+
+displayDistance=DistanceVisualiser()
+
+myCallBack=CallBack()
+LogChannel = "44697374616e6365"
+LogSubscriber = subscriber.SubscriberEventChannel(LogChannel)
+LogSubscriber.subscribe(LogChannel, myCallBack.LogCallback)
+
+## Asyncore Loop & Signal Handling
+print "Mit STRG + C beenden."
+asyncore.loop()
+#while True:
+#    asyncore.loop(10,True,None,1)
+#    time.sleep(0.01)
+#
