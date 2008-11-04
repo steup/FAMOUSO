3b56a5f8a90be6bb3f460bd82504f854a086cc41
zug <zug@235a714a-1e17-0410-b3bb-947d69a96898>
2008-11-04 14:07:45 +0000

    - new structure implemented
    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@664 235a714a-1e17-0410-b3bb-947d69a96898

---
+++ examples/MultiRobotDemo/Matlab/simrobot/control.m
@@ -0,0 +1,85 @@
+function varargout = control(varargin)
+%CONTROL M-file for control.fig
+%      CONTROL, by itself, creates a new CONTROL or raises the existing
+%      singleton*.
+%
+%      H = CONTROL returns the handle to a new CONTROL or the handle to
+%      the existing singleton*.
+%
+%      CONTROL('Property','Value',...) creates a new CONTROL using the
+%      given property value pairs. Unrecognized properties are passed via
+%      varargin to control_OpeningFcn.  This calling syntax produces a
+%      warning when there is an existing singleton*.
+%
+%      CONTROL('CALLBACK') and CONTROL('CALLBACK',hObject,...) call the
+%      local function named CALLBACK in CONTROL.M with the given input
+%      arguments.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help control
+
+% Last Modified by GUIDE v2.5 04-Nov-2008 09:03:38
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @control_OpeningFcn, ...
+                   'gui_OutputFcn',  @control_OutputFcn, ...
+                   'gui_LayoutFcn',  [], ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+   gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+% --- Executes just before control is made visible.
+function control_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   unrecognized PropertyName/PropertyValue pairs from the
+%            command line (see VARARGIN)
+
+% Choose default command line output for control
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes control wait for user response (see UIRESUME)
+% uiwait(handles.controlfigure);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = control_OutputFcn(hObject, eventdata, handles)
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+% --- Executes on button press in go.
+function go_Callback(hObject, eventdata, handles)
+% hObject    handle to go (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% --- Executes on button press in stop.
+function stop_Callback(hObject, eventdata, handles)
+% hObject    handle to stop (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
