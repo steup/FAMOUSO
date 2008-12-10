85bd3f414e9e9c2fbf4d5fd0a4bd4bec34759860
mschulze <mschulze@235a714a-1e17-0410-b3bb-947d69a96898>
2008-12-10 14:30:52 +0000

    -two new test programms that explain the use of  famouso for C++ more generally and cleaner
    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@826 235a714a-1e17-0410-b3bb-947d69a96898

---
+++ Bindings/C++/Publisher.cc
@@ -0,0 +1,38 @@
+/*
+ * Publisher.cc
+ *
+ * Michael Schulze, 2008
+ */
+
+#include <iostream>
+#include <boost/thread/thread.hpp>
+#include <boost/thread/xtime.hpp>
+
+// the include contains the famouso
+// configuration for local event
+// propagation
+#include "famouso_bindings.h"
+
+int main(int argc, char **argv) {
+
+    // create a PublisherEventChannel
+    // with a specific Subject
+    famouso::config::PEC pec(0xf100000000000000ull);
+    // announce the channel
+    pec.announce();
+
+    famouso::mw::Event e(pec.subject());
+    e.length = 7;
+    e.data = (uint8_t*)"Publish";
+
+    // busy waiting is possible, however giving up
+    // the cpu is much more better for other processes
+    // that have something to do
+    while (1) {
+        pec.publish(e);
+        boost::xtime time;
+        boost::xtime_get( &time, boost::TIME_UTC );
+        time.sec += 1;
+        boost::thread::sleep( time );
+    }
+}
---
+++ Bindings/C++/Subscriber.cc
@@ -0,0 +1,47 @@
+/*
+ * Subscriber.cc
+ *
+ *
+ *  Michael Schulze, 2008
+ */
+
+#include <iostream>
+#include <boost/thread/thread.hpp>
+#include <boost/thread/xtime.hpp>
+
+// the include contains the famouso
+// configuration for local event
+// propagation
+#include "famouso_bindings.h"
+
+// definition of a simple event callback that
+// prints the received event data out
+void cb(famouso::mw::api::SECCallBackData& cbd) {
+    std::cout << __PRETTY_FUNCTION__ << " Length="
+              << cbd.length << " Event data="
+              << cbd.data << std::endl;
+}
+
+int main(int argc, char **argv) {
+
+    famouso::init<famouso::config>();
+
+    // create a SubscriberEventChannel
+    // with a specific Subject
+    famouso::config::SEC sec(0xf100000000000000ull);
+    // subscribe and register the respective
+    // callback that is called if an event
+    // occurs
+    sec.subscribe();
+    sec.callback.bind<cb>();
+
+    // busy waiting is possible, however giving up
+    // the cpu is much more better for other processes
+    // that have something to do
+    while (1) {
+        boost::xtime time;
+        boost::xtime_get( &time, boost::TIME_UTC );
+        time.sec += 100;
+        boost::thread::sleep( time );
+    }
+}
