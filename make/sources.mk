1e3f693af6e914830133525b0dc52f14de2bf873
mschulze <mschulze@235a714a-1e17-0410-b3bb-947d69a96898>
2009-01-30 16:27:14 +0000

    new generic CAN rx_Interrupt delegate defined
    The generic CAN rx_Interrupt serves as entry point to the
    CAN-Network Layer independently of the underlying concrete
    CAN-Driver. The real interrupt source of the CAN-Driver
    delegates to the generic interrupt and then we are able
    to process the CAN-Message in a streamlined way.
    
    
    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@918 235a714a-1e17-0410-b3bb-947d69a96898

--- make/sources.mk
+++ make/sources.mk
@@ -9,7 +9,8 @@ src/mw/common/AttributeList.cc \
 src/mw/common/Attribute.cc \
 src/mw/common/Event.cc \
 src/mw/common/CallBack.cc \
-src/mw/el/EventLayerCallback.cc
+src/mw/el/EventLayerCallback.cc \
+src/mw/nl/can/GenericReceiveInterrupt.cc
 
 #$(shell find $(SRCDIR) -name "*.cc")
 TOOLSRC  = $(shell find $(TOOLSDIR) -name "*.cc")
---
+++ src/mw/nl/can/GenericReceiveInterrupt.cc
@@ -0,0 +1,10 @@
+#include "case/Delegate.h"
+namespace device {
+  namespace nic {
+    namespace CAN {
+        namespace detail {
+            famouso::util::Delegate<> rx_Interrupt;
+      } /*  namespace detail */
+    } /* namespace CAN */
+  } /* namespace nic */
+} /* namespace device */
