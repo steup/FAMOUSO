6c72259d4aa726afaf831b5bc78595701ce30ab8
zug <zug@235a714a-1e17-0410-b3bb-947d69a96898>
2009-02-11 09:46:03 +0000

    unnecessary differentiation between simulated and real robots during update cycle eleminated
    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@986 235a714a-1e17-0410-b3bb-947d69a96898

--- examples/MultiRobotDemo/Simulator/simrobot/step.m
+++ examples/MultiRobotDemo/Simulator/simrobot/step.m
@@ -5,7 +5,7 @@ robot=scenario.robots(i);
 
 if ~robot.power && ~robot.crashed;
     scenario.robots(i)=activate(scenario.robots(i),scenario.mode);
-    disp(['Robot ' num2str(i) ' activated ...']);
+    disp(['Robot ' num2str(scenario.robots(i).number) ' activated ...']);
     scenario.robots(i).power=true;
     if isempty(handles)
         handles=guihandles(findobj('name','control'));
@@ -20,19 +20,8 @@ end
 
 %% Execute robots' algorithms
 % Take robot from the list and execute algorithm
-if strcmp(scenario.robots(i).trigger.triggerMode,'timer')
-    scenario.robots(i) = evaluate(scenario.robots(i), i ,scenario.matrix);
-else
-    [dist,num] = readusonic(scenario.robots(i),i,'sensor_1',scenario.matrix);
-   if scenario.FAMOUSO==1
-       global distance;
-       aux=dist;
-       if aux>=255
-           aux=255;
-       end
-       publishing(distance,[aux 118 i]);
-   end
-end
+scenario.robots(i) = evaluate(scenario.robots(i), i ,scenario.matrix);
+
 % Update robot
 scenario.robots(i) = update(i,scenario);
 
@@ -45,12 +34,12 @@ if scenario.robots(i).crashed && scenario.robots(i).power
             stop(aux(in));
             delete(aux(in));
             % local controlled robot
-            fprintf('Robot %i crashed - corresponding timer switched off \n',i);
+            fprintf('Robot %i crashed - corresponding timer switched off \n',scenario.robots(i).number);
             aux=get(handles.Active,'String');
             set(handles.Active,'String',num2str(str2double(aux)-1));      
         else
             % via FAMOUSO controlled robot
-            fprintf('Robot %i crashed \n',i);
+            fprintf('Robot %i crashed \n',scenario.robots(i).number);
             handles=findobj('Tag','Active');
             set(handles,'String',num2str(str2double(aux)-1));   
         end
