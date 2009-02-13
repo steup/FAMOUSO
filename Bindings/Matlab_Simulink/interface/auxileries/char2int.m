a5f6d621b4cec0f868f6f296ff78d209d1d9c66b
zug <zug@235a714a-1e17-0410-b3bb-947d69a96898>
2009-02-13 08:06:48 +0000

    quite often used char2int.m script condensed
    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@999 235a714a-1e17-0410-b3bb-947d69a96898

--- Bindings/Matlab_Simulink/interface/auxileries/char2int.m
+++ Bindings/Matlab_Simulink/interface/auxileries/char2int.m
@@ -1,8 +1,10 @@
 function value=char2int(data)
-    for i=1:max(size(data))
-        if data(i)>127
-            value(i)=double(data(i))-256;
-        else
-            value(i)=double(data(i)); 
-        end
-    end
+%     for i=1:max(size(data))
+%         if data(i)>127
+%             value(i)=double(data(i))-256;
+%         else
+%             value(i)=double(data(i)); 
+%         end
+%     end
+    value=double(data);
+    value(value>127)=value(value>127)-256;
