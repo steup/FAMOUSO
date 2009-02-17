e8de06308649d632bcff7e9fa89af28134a0c346
mschulze <mschulze@235a714a-1e17-0410-b3bb-947d69a96898>
2009-02-17 09:57:33 +0000

    correct output message in order to give the right information
    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@1018 235a714a-1e17-0410-b3bb-947d69a96898

--- include/mw/el/EventLayerMiddlewareStub.h
+++ include/mw/el/EventLayerMiddlewareStub.h
@@ -198,7 +198,7 @@ namespace el {
                     ecc->start();
                     start_accept();
                 } else {
-                    std::cerr << "Error in asynchronous acceptance of an incoming connection" << std::endl;
+                    std::cerr << "Error in asynchronous acceptance of an incoming connection or CTRL^C" << std::endl;
                 }
             }
             boost::asio::ip::tcp::acceptor acceptor_;
