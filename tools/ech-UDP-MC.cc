d9c549d1e6288f9ce46481021b498058f1cf08d5
mschulze <mschulze@235a714a-1e17-0410-b3bb-947d69a96898>
2009-02-24 12:15:34 +0000

    local ech tool for the UDPMultiCastNL
    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@1046 235a714a-1e17-0410-b3bb-947d69a96898

---
+++ tools/ech-UDP-MC.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+
+// UDPBroadCastNL specific
+#include "mw/nl/UDPMultiCastNL.h"
+
+// common famouso middlware includes
+#include "mw/anl/AbstractNetworkLayer.h"
+#include "mw/el/EventLayer.h"
+#include "mw/el/EventLayerMiddlewareStub.h"
+#include "mw/api/PublisherEventChannel.h"
+#include "mw/api/SubscriberEventChannel.h"
+#include "mw/common/Event.h"
+
+#include "famouso.h"
+
+#include "util/Idler.h"
+
+namespace famouso {
+    namespace UDP {
+        class config {
+                typedef famouso::mw::nl::UDPMultiCastNL nl;
+                typedef famouso::mw::anl::AbstractNetworkLayer< nl > anl;
+                typedef famouso::mw::el::EventLayer< anl > el;
+            public:
+                typedef famouso::mw::el::EventLayerMiddlewareStub< el > ELMS;
+                typedef famouso::mw::api::EventChannel< el > EC;
+                typedef famouso::mw::api::PublisherEventChannel<el> PEC;
+                typedef famouso::mw::api::SubscriberEventChannel<el> SEC;
+        };
+    }
+
+    typedef UDP::config config;
+}
+
+int main (int argc, char **argv) {
+    std::cout << "Project: FAMOUSO" << std::endl;
+    std::cout << "local Event Channel Handler" << std::endl << std::endl;
+    std::cout << "Author: Michael Schulze" << std::endl;
+    std::cout << "Revision: $Rev$" << std::endl;
+    std::cout << "$Date$" << std::endl;
+    std::cout << "last changed by $Author$" << std::endl << std::endl;
+    std::cout << "build Time: " << __TIME__ << std::endl;
+    std::cout << "build Date: " << __DATE__ << std::endl << std::endl;
+
+    try {
+        famouso::init<famouso::config::EC>();
+        famouso::config::ELMS localELMS;
+        std::cout << "FAMOUSO -- Initalisation successfull" << std::endl << std::endl;
+        Idler::idle();
+    } catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    std::cout << "FAMOUSO -- successfully finished" << std::endl;
+    return 0;
+}
+
