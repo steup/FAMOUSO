e8c6df0f20d937aa630066957f543d2cd2470543
mschulze <mschulze@235a714a-1e17-0410-b3bb-947d69a96898>
2009-03-30 11:25:48 +0000

    the determination of endianness should now work for all supported platforms, hopefully (closes #21)
    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@1122 235a714a-1e17-0410-b3bb-947d69a96898

--- include/util/endianness.h
+++ include/util/endianness.h
@@ -5,46 +5,39 @@
 
 #if defined(LINUX)
     #include <arpa/inet.h>
+    #if __BYTE_ORDER == __BIG_ENDIAN
+    // The host byte order is the same as network byte order,
+    // so these functions are all just identity.
+        # define htonll(x)  (x)
+        # define ntohll(x)  (x)
+    #else
+        #if __BYTE_ORDER == __LITTLE_ENDIAN
+            #define htonll(x)    __swap_endianness_64(x)
+            #define ntohll(x)    __swap_endianness_64(x)
+        #else
+            #error "BYTE_ORDER is not defined"
+        #endif
+    #endif
 
-#elif defined(WIN32)
-    #include <winsock2.h>
-// local definition explanation see below
-#   define htonll(x)    __swap_endianness_64(x)
-#   define ntohll(x)    __swap_endianness_64(x)
 
-#elif defined(AVR)
-    #define __BYTE_ORDER __LITTLE_ENDIAN
-    #define ntohl(x) __swap_endianness_32(x)
-    #define ntohs(x) __swap_endianness_16(x)
-    #define htonl(x) __swap_endianness_32(x)
-    #define htons(x) __swap_endianness_16(x)
+#elif defined(WIN32) || defined(AVR)
+    // Windows and AVR are both little endian
+    #if defined(WIN32)
+        #include <winsock2.h>
+    #endif
+
+    #if defined(AVR)
+        #define ntohl(x) __swap_endianness_32(x)
+        #define ntohs(x) __swap_endianness_16(x)
+        #define htonl(x) __swap_endianness_32(x)
+        #define htons(x) __swap_endianness_16(x)
+    #endif
+
+    #define htonll(x)    __swap_endianness_64(x)
+    #define ntohll(x)    __swap_endianness_64(x)
 
 #else
-#error "It seems to be compiling for an unsupported platform and it needs to be defined the endianness of that system."
-
-#endif
-
-
-// excluding WIN-Compiler from doing the following
-// is reasoned by the fact that the preprocessor
-// executes the wrong statement or misinterpretes 
-// the defines if we use the -mno-cygwin option. 
-#ifndef WIN32
-
-#if __BYTE_ORDER == __BIG_ENDIAN
-///* The host byte order is the same as network byte order,
-//   so these functions are all just identity.  */
-# define htonll(x)  (x)
-# define ntohll(x)  (x)
-# else
-#  if __BYTE_ORDER == __LITTLE_ENDIAN
-#   define htonll(x)    __swap_endianness_64(x)
-#   define ntohll(x)    __swap_endianness_64(x)
-#  else
-#   error "BYTE_ORDER is not defined"
-#  endif
-# endif
-
+    #error "It seems to be compiling for an unsupported platform and it needs to be defined the endianness of that system."
 #endif
 
 #endif
