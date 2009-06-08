523490fdda4f587f5f137097a2e02304a28c9bdb
mschulze <mschulze@235a714a-1e17-0410-b3bb-947d69a96898>
2009-06-08 14:38:57 +0000

    add an ECH with CAN network layer and SocketCAN-Driver support
    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@1238 235a714a-1e17-0410-b3bb-947d69a96898

---
+++ tools/ech-SocketCAN.cc
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2008, 2009 Michael Schulze <mschulze@ivs.cs.uni-magdeburg.de>
+ * All rights reserved.
+ *
+ *    Redistribution and use in source and binary forms, with or without
+ *    modification, are permitted provided that the following conditions
+ *    are met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *
+ *    * Neither the name of the copyright holders nor the names of
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ *
+ *    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ *    IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ *    TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ *    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *    OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * $Id$
+ *
+ ******************************************************************************/
+
+
+// CAN-NL specific includes
+#include "devices/nic/can/SocketCAN/SocketCAN.h"
+#include "mw/nl/can/etagBP/Broker.h"
+#include "mw/nl/can/ccp/Broker.h"
+#include "mw/nl/CANNL.h"
+
+// common famouso middlware includes
+#include "mw/anl/AbstractNetworkLayer.h"
+#include "mw/el/EventLayer.h"
+#include "mw/el/EventLayerMiddlewareStub.h"
+#include "mw/api/PublisherEventChannel.h"
+#include "mw/api/SubscriberEventChannel.h"
+
+#include "config/generator/description.h"
+
+namespace famouso {
+    class config {
+        typedef device::nic::CAN::SocketCAN can;
+        typedef famouso::mw::nl::CAN::ccp::Broker<can> ccpBroker;
+        typedef famouso::mw::nl::CAN::etagBP::Broker<can> etagBroker;
+        typedef famouso::mw::nl::CANNL<can, ccpBroker, etagBroker> nl;
+        typedef famouso::mw::anl::AbstractNetworkLayer< nl > anl;
+      public:
+        typedef famouso::mw::el::EventLayer< anl > EL;
+
+        typedef famouso::mw::el::EventLayerMiddlewareStub< EL > ELMS;
+
+        typedef famouso::mw::api::PublisherEventChannel<EL> PEC;
+        typedef famouso::mw::api::SubscriberEventChannel<EL> SEC;
+
+        DESCRIPTION("Project: FAMOUSO\n"
+                    "local Event Channel Handler with a CAN network layer and a SocketCAN-Driver\n\n"
+                    "Author: Michael Schulze\n"
+                    "Revision: $Rev$\n"
+                    "$Date$\n"
+                    "last changed by $Author$\n\n"
+                    "build Time: "__TIME__"\n"
+                    "build Date: "__DATE__);
+    };
+}
+
+#include "generic-main.impl"
+
