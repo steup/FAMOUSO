720414401608a60ce9d97d87d01d5c9653034f67
mschulze <mschulze@235a714a-1e17-0410-b3bb-947d69a96898>
2009-07-17 09:58:19 +0000

    GenericReveiveInterrupt removed for the CAN, because it is now handled in another way
    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@1333 235a714a-1e17-0410-b3bb-947d69a96898

--- make/sources.mk
+++ make/sources.mk
@@ -48,8 +48,7 @@ src/mw/common/AttributeList.cc \
 src/mw/common/Attribute.cc \
 src/mw/common/Event.cc \
 src/mw/common/CallBack.cc \
-src/mw/el/EventLayerCallback.cc \
-src/mw/nl/can/GenericReceiveInterrupt.cc
+src/mw/el/EventLayerCallback.cc
 
 #$(shell find $(SRCDIR) -name "*.cc")
 TOOLSRC  = $(shell find $(TOOLSDIR) -name "*.cc")
