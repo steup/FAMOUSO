ad8a705169318f55f843fc87cc41a7c5ecd16e70
mschulze <mschulze@235a714a-1e17-0410-b3bb-947d69a96898>
2009-11-11 22:15:26 +0000

    enhancing delegate to enable specification of return type
    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@1471 235a714a-1e17-0410-b3bb-947d69a96898

--- include/case/Delegate.h
+++ include/case/Delegate.h
@@ -47,59 +47,59 @@ namespace famouso {
          *  \class Delegate
          *  \brief Implements anonymous callback functionality
          */
-        template < typename parameter = void >
+        template < typename parameter=void, typename R=void >
         class Delegate {
-                typedef void (*invoke_stub)(void const *, parameter);
+                typedef R (*invoke_stub)(void const *, parameter);
                 void const  *obj_ptr_;
                 invoke_stub stub_ptr_;
 
-                template < typename T, void (T::*Fxn)(parameter) >
+                template < typename T, R (T::*Fxn)(parameter) >
                 struct mem_fn_stub {
-                    static void invoke(void const * obj_ptr, parameter a0) {
+                    static R invoke(void const * obj_ptr, parameter a0) {
                         T * obj = static_cast<T *>(const_cast<void *>(obj_ptr));
-                        (obj->*Fxn)(a0);
+                        return (obj->*Fxn)(a0);
                     }
                 };
 
-                template < typename T, void (T::*Fxn)(parameter) const >
+                template < typename T, R (T::*Fxn)(parameter) const >
                 struct mem_fn_const_stub {
-                    static void invoke(void const * obj_ptr, parameter a0) {
+                    static R invoke(void const * obj_ptr, parameter a0) {
                         T const * obj = static_cast<T const *>(obj_ptr);
-                        (obj->*Fxn)(a0);
+                        return (obj->*Fxn)(a0);
                     }
                 };
 
-                template < void (*Fxn)(parameter) >
+                template < R (*Fxn)(parameter) >
                 struct function_stub {
-                    static void invoke(void const *, parameter a0) {
-                        (*Fxn)(a0);
+                    static R invoke(void const *, parameter a0) {
+                        return (*Fxn)(a0);
                     }
                 };
 
             public:
                 Delegate() : obj_ptr_(0), stub_ptr_(0) { }
 
-                template < typename T, void (T::*Fxn)(parameter) >
+                template < typename T, R (T::*Fxn)(parameter) >
                 void bind(T * obj) {
                     obj_ptr_ = const_cast<T const *>(obj);
                     stub_ptr_ = &mem_fn_stub<T, Fxn>::invoke;
                 }
 
-                template < typename T, void (T::*Fxn)(parameter) const >
+                template < typename T, R (T::*Fxn)(parameter) const >
                 void bind(T const * obj) {
                     obj_ptr_ = obj;
                     stub_ptr_ = &mem_fn_const_stub<T, Fxn>::invoke;
                 }
 
-                template < void (*Fxn)(parameter) >
+                template < R (*Fxn)(parameter) >
                 void bind() {
                     obj_ptr_ = 0;
                     stub_ptr_ = &function_stub<Fxn>::invoke;
                 }
 
                 //  additional attributes only support on newer compilers
-                void operator()(parameter a0) const {
-                    (*stub_ptr_)(obj_ptr_, a0);
+                R operator()(parameter a0) const {
+                    return (*stub_ptr_)(obj_ptr_, a0);
                 }
 
                 operator bool() const {
@@ -117,7 +117,7 @@ namespace famouso {
                 struct mem_fn_stub {
                     static void invoke(void const * obj_ptr) {
                         T * obj = static_cast<T *>(const_cast<void *>(obj_ptr));
-                        (obj->*Fxn)();
+                        return (obj->*Fxn)();
                     }
                 };
 
@@ -125,14 +125,14 @@ namespace famouso {
                 struct mem_fn_const_stub {
                     static void invoke(void const * obj_ptr) {
                         T const * obj = static_cast<T const *>(obj_ptr);
-                        (obj->*Fxn)();
+                        return (obj->*Fxn)();
                     }
                 };
 
                 template < void (*Fxn)() >
                 struct function_stub {
                     static void invoke(void const *) {
-                        (*Fxn)();
+                        return (*Fxn)();
                     }
                 };
 
@@ -159,7 +159,7 @@ namespace famouso {
 
                 //  additional attributes only support on newer compilers
                 void operator()() const {
-                    (*stub_ptr_)(obj_ptr_);
+                    return (*stub_ptr_)(obj_ptr_);
                 }
 
                 operator bool() const {
