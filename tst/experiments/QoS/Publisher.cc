f52f94d2615a1a33ae06685c8c9f52e7197a4ad0
kriese <kriese@235a714a-1e17-0410-b3bb-947d69a96898>
2010-03-17 15:23:05 +0000

    Added experiments for QoS checking. (refs #50)
    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@1547 235a714a-1e17-0410-b3bb-947d69a96898

---
+++ tst/experiments/QoS/Publisher.cc
@@ -0,0 +1,81 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2008-2010 Michael Schulze <mschulze@ivs.cs.uni-magdeburg.de>
+ *               2009-2010 Michael Kriese <kriese@cs.uni-magdeburg.de>
+ * All rights reserved.
+ *
+ *    Redistribution and use in source and binary forms, with or without
+ *    modification, are permitted provided that the following conditions
+ *    are met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *
+ *    * Neither the name of the copyright holders nor the names of
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ *
+ *    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ *    IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ *    TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ *    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *    OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * $Id$
+ *
+ ******************************************************************************/
+
+/*
+ * Publisher.cc
+ *
+ * Michael Schulze, 2008
+ */
+
+#include "debug.h"
+#include <boost/thread/thread.hpp>
+#include <boost/thread/xtime.hpp>
+
+// the include contains the famouso
+// configuration for local event
+// propagation
+#include "config.h"
+
+int main(int argc, char **argv) {
+
+    famouso::init<famouso::config>();
+
+    // create a PublisherEventChannel
+    // with a specific Subject
+    famouso::config::PEC pec(famouso::mw::Subject(SUBJECT));
+
+    // announce the channel
+    pec.announce();
+
+    famouso::mw::Event e(pec.subject());
+    e.length = 7;
+    e.data = (uint8_t*)"Publish";
+
+    // busy waiting is possible, however giving up
+    // the cpu is much more better for other processes
+    // that have something to do
+    while (1) {
+        pec.publish(e);
+        boost::xtime time;
+        boost::xtime_get(&time, boost::TIME_UTC);
+        time.sec += 1;
+        boost::thread::sleep(time);
+    }
+}
