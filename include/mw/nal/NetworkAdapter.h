steup/FAMOUSO

commit 98b0b601fca5e4dc505da1fad94502f5cabcb70c

Integrates AFP into the abstract network layer
- every network layer can define an own AFP config by adding a specialization
  in include/mw/anl/AFPConfig.h which is used as default for this network layer
- the AbstractNetworkLayer gets two optional template parameters, that allow
  to overwrite the default AFP config or to disable the usage of AFP
- the Packet data structure contains the information whether a packet is a
  fragment or a complete event; the network layer is responsible to transmit
  this information (already implemented for UDPMultiCastNL and AWDSNL);
  the network layer function deliver_fragment() is not needed anymore
- adds test programs

git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@1606 235a714a-1e17-0410-b3bb-947d69a96898

include/mw/nal/NetworkAdapter.h
@@ -192,6 +192,10 @@ namespace famouso {
                      *  \param[in]  bnl the sub network-ID from where the request came.
                      */
                     void event_process_request(famouso::mw::nl::BaseNL * const bnl) {
+                        if (_ANL_A.id() == bnl)
+                            return _ANL_A.event_process_request(bnl);
+                        else
+                            return _ANL_B.event_process_request(bnl);
                         _bnl = bnl;
                     }
 
@@ -199,6 +203,10 @@ namespace famouso {
                      *         the event is processed now.
                      */
                     void event_processed() {
+                        if (_ANL_A.id() == _bnl)
+                            return _ANL_A.event_processed();
+                        else
+                            return _ANL_B.event_processed();
                         _bnl = 0;
                     }
             };

include/mw/anl/AFPConfig.h
@@ -1,6 +1,7 @@
 /*******************************************************************************
  *
- * Copyright (c) 2008-2010 Michael Schulze <mschulze@ivs.cs.uni-magdeburg.de>
+ * Copyright (c) 2010 Philipp Werner <philipp.werner@st.ovgu.de>
+ *                    Michael Schulze <mschulze@ivs.cs.uni-magdeburg.de>
  * All rights reserved.
  *
  *    Redistribution and use in source and binary forms, with or without
@@ -37,72 +38,71 @@
  *
  ******************************************************************************/
 
-#ifndef __voidNL_h__
-#define __voidNL_h__
+#ifndef __AFPCONFIG_H_6E4D6E109554C6__
+#define __AFPCONFIG_H_6E4D6E109554C6__
 
-#include "mw/nl/BaseNL.h"
-#include "mw/nl/Packet.h"
-#include "mw/common/NodeID.h"
-#include "mw/common/Subject.h"
-#include "mw/api/EventChannel.h"
-#include "debug.h"
-#include <stdio.h>
+#include "mw/afp/Config.h"
 
 namespace famouso {
     namespace mw {
         namespace nl {
 
-            class voidNL : public BaseNL {
-                public:
-                    struct info {
-                        enum {
-                            payload = 65534
-                        };
-                    };
+            template< class CAN_Driver, class CCP, class BP >
+            class CANNL;
 
-                    typedef uint16_t SNN;
+            class voidNL;
 
-                    typedef Packet<SNN> Packet_t;
-
-
-                    voidNL() {}
-                    ~voidNL() {}
-
-                    void init() {
-                        init(UID());
-                    }
-                    void init(const NodeID &i) {
-                        TRACE_FUNCTION;
-                        ::logging::log::emit< ::logging::Info>()
-                            << " Configuration 64Bit NodeID=" << i.value()
-                            << ::logging::log::endl;
-                    }
-
-                    // bind Subject to specific networks name
-                    void bind(const Subject &s, SNN &snn) {
-                        TRACE_FUNCTION;
-                        snn = 0x1;
-                    }
-
-                    void deliver(const Packet_t& p) {
-                        TRACE_FUNCTION;
-                    }
-
-                    void fetch(Packet_t& p) {
-                        TRACE_FUNCTION;
-                    }
-
-                    void interrupt() {
-                        TRACE_FUNCTION;
-                    }
-
-                    SNN lastPacketSNN() {
-                        return 0xff;
-                    }
-            };
         } // namespace nl
+
+
+        namespace anl {
+
+            /*!
+             *  \brief  Base class for all ANL AFP configs
+             *  \tparam SNN     type to distinguish between different
+             *                  fragment sources (subjects)
+             */
+            template < typename SNN >
+            struct AFPBaseConfig : afp::DefaultConfig {
+                enum {
+                    multiple_subjects = true
+                };
+                typedef SNN SubjectType;
+            };
+
+            /*!
+             *  \brief  Default ANL AFP config (all network layers without specialization)
+             */
+            template < class NL >
+            struct AFPConfig {
+                struct type : AFPBaseConfig<typename NL::SNN> {
+                    enum {
+                        event_seq = true,
+                        reordering = true,
+                        duplicates = true
+                    };
+                };
+            };
+
+            /*!
+             *  \brief  Default ANL AFP config (CANNL)
+             */
+            template < class CD, class CCP, class BP>
+            struct AFPConfig< nl::CANNL<CD,CCP,BP> > {
+                typedef AFPBaseConfig<typename nl::CANNL<CD,CCP,BP>::SNN> type;
+            };
+
+            /*!
+             *  \brief  Default ANL AFP config (voidNL)
+             */
+            template <>
+            struct AFPConfig<nl::voidNL> {
+                typedef afp::Disable type;
+            };
+
+        } // namespace anl
     } // namespace mw
 } //namespace famouso
 
-#endif /* __voidNL_h__ */
+#endif // __AFPCONFIG_H_6E4D6E109554C6__
 

include/mw/anl/AbstractNetworkLayer.h
@@ -1,6 +1,7 @@
 /*******************************************************************************
  *
  * Copyright (c) 2008-2010 Michael Schulze <mschulze@ivs.cs.uni-magdeburg.de>
+ *                    2010 Philipp Werner <philipp.werner@st.ovgu.de>
  * All rights reserved.
  *
  *    Redistribution and use in source and binary forms, with or without
@@ -43,6 +44,9 @@
 #include "debug.h"
 #include "mw/common/Event.h"
 #include "mw/nl/BaseNL.h"
+#include "mw/anl/AFPConfig.h"
+#include "mw/afp/Fragmenter.h"
+#include "mw/afp/Defragmentation.h"
 
 namespace famouso {
     namespace mw {
@@ -56,14 +60,21 @@ namespace famouso {
              *          also the aspects of quality of service or attribute management.
              *
              *  \tparam NL the network layer see prerequisites.
+             *  \tparam AFP_FragConfig  the AFP fragmentation %config, see \ref afp_config
+             *  \tparam AFP_DefragConfig  the AFP defragmentation %config, see \ref afp_config
              *
              *  \pre    The type of template parameters can be an famouso::mw::nl::CANNL,
              *          famouso::mw::nl::AWDSNL, famouso::mw::nl::UDPMultiCastNL,
              *          famouso::mw::nl::UDPBroadCastNL or an famouso::mw::nl::VoidNL dependent
-             *          on the configuration of the middleware stack
+             *          on the %configuration of the middleware stack
              */
-            template < class NL >
+            template < class NL, class AFP_FragConfig = typename AFPConfig<NL>::type, class AFP_DefragConfig = typename AFPConfig<NL>::type >
             class AbstractNetworkLayer : public NL {
+
+                    /*! \brief  defragmentation data and algorithms
+                     */
+                    afp::DefragmentationProcessorANL<AFP_DefragConfig> defrag;
+
                 public:
 
                     /*! \brief  short network representation of the subject
@@ -74,6 +85,12 @@ namespace famouso {
                      *          that is used for announcing subscribtion network-wide
                      */
                     SNN   subscribe_SNN;
+
+                    /*! \brief  constructor
+                     */
+                    AbstractNetworkLayer() :
+                            defrag(NL::info::payload) {
+                    }
 
                     /*! \brief Initalizes the sub networks and bind the subscription
                      *         management channel.
@@ -100,16 +117,39 @@ namespace famouso {
                      *
                      *  \param[in]  snn the short network name of the subject
                      *  \param[in]  e the event that has to be published
+                     *
+                     *  \todo   Save copy operation in fragmentation case (needs
+                     *          AFP interface extension returning AFP header and
+                     *          payload separately and Packet/NL adaption)
                      */
                     void publish(const SNN &snn, const Event &e) {
                         TRACE_FUNCTION;
-                        typename NL::Packet_t p(snn, &e[0], e.length);
-                        if (e.length <= NL::info::payload)
+                        if (e.length <= NL::info::payload) {
+                            typename NL::Packet_t p(snn, &e[0], e.length);
                             NL::deliver(p);
-                        else
-                            ::logging::log::emit< ::logging::Warning>()
-                                << "Event is to big to deliver at once and fragmentation"
-                                << "is not supported at the moment" << ::logging::log::endl;
+                        } else {
+                            // Fragmentation using AFP (if disabled, a warning is emitted to the log)
+                            typedef afp::Fragmenter<AFP_FragConfig, NL::info::payload> Frag;
+
+                            if (e.length != (typename Frag::elen_t) e.length) {
+                                ::logging::log::emit< ::logging::Warning>()
+                                    << "AFP: Cannot publish event... too large."
+                                    << ::logging::log::endl;
+                                return;
+                            }
+
+                            Frag frag(e.data, e.length);
+
+                            if (frag.error())
+                                return;
+
+                            uint8_t buffer [NL::info::payload];
+                            typename Frag::flen_t length;
+                            while ( (length = frag.get_fragment(buffer)) ) {
+                                typename NL::Packet_t p(snn, buffer, length, true);
+                                NL::deliver(p);
+                            }
+                        }
                     }
 
                     /*! \brief  subscribe a subject and get its short network representation
@@ -136,7 +176,9 @@ namespace famouso {
                      *          name is equal to the short network name of the arosen packet.
                      *
                      *  \param[in]  snn the short network name of the subject
-                     *  \param[out] e the event that has to be published
+                     *  \param[out] e   The event that has to be published. If there is no event to
+                     *                  fetch (the arosen packet was a fragment not completing an
+                     *                  event), e.data is set to NULL.
                      *  \param[in]  bnl the sub network in that the event \e e will be published.
                      *
                      *  \return \li \b true if \e snn and the snn of the last arosen packet are equal
@@ -147,8 +189,20 @@ namespace famouso {
                         if (snn == NL::lastPacketSNN()) {
                             typename NL::Packet_t p;
                             NL::fetch(p);
-                            e.length = p.data_length;
-                            e.data = p.data;
+                            if (!p.fragment) {
+                                e.length = p.data_length;
+                                e.data = p.data;
+                            } else {
+                                // Apply AFP
+                                afp::DefragmentationStep<AFP_DefragConfig> ds(p.data, p.data_length, NL::lastPacketSNN());
+                                defrag.process_fragment(ds);
+                                if (ds.event_complete()) {
+                                    e.data = ds.get_event_data();
+                                    e.length = ds.get_event_length();
+                                } else {
+                                    e.data = 0;
+                                }
+                            }
                             return true;
                         } else {
                             return false;
@@ -167,6 +221,7 @@ namespace famouso {
                      *         the event is processed now.
                      */
                     void event_processed() {
+                        defrag.last_event_processed();
                     }
 
             };

include/mw/el/EventLayer.h
@@ -208,8 +208,12 @@ namespace famouso {
                                 // if event was fetched, meaning we have a matching
                                 // of subjects, publish it locally to the respective
                                 // subscribers and then we are done.
-                                publish_local(e);
-                                return;
+
+                                // If the packet was a fragment the event may not be complete
+                                if (e.data)
+                                    publish_local(e);
+
+                                break;
                             }
                             sec = static_cast<ec_t*>(sec->select());
                         }

include/mw/nl/AWDSNL.h
@@ -115,19 +115,8 @@ namespace famouso {
                      * Sends the given packet.
                      *
                      * \param[in] p packet
-                     * \param[in] type describes the packet type and is used for specifying
-                     *            a fragmented or normal published packet.
                      */
-                    void deliver(const Packet_t& p, uint8_t type = AWDS_Packet::constants::packet_type::publish);
-
-                    /**
-                     * \brief deliver a fragment of a packet.
-                     *
-                     * Sends the given packet.
-                     *
-                     * \param p packet
-                     */
-                    void deliver_fragment(const Packet_t& p);
+                    void deliver(const Packet_t& p);
 
                     /**
                      * \brief processes incoming packets

include/mw/nl/CANNL.h
@@ -81,6 +81,7 @@ namespace famouso {
 
                     typedef Packet<SNN> Packet_t;
 
+
                     CANNL() : tx_node(0) {}
                     ~CANNL() {}
 
@@ -109,6 +110,9 @@ namespace famouso {
                         snn = etagBP.bind_subject(s, tx_node, driver);
                     }
 
+                    /*!
+                     *  \todo   transmit p.fragment
+                     */
                     void deliver(const Packet_t& p) {
                         // senden des CAN Paketes
                         TRACE_FUNCTION;
@@ -126,11 +130,9 @@ namespace famouso {
                         driver.transmit(m);
                     }
 
-                    void deliver_fragment(const Packet_t& p) {
-                        // Fragmentierung wird zur Zeit noch nicht unterstuetzt
-                        TRACE_FUNCTION;
-                    }
-
+                    /*!
+                     *  \todo   transmit p.fragment
+                     */
                     void fetch(Packet_t& p) {
                         // ist das Auslesen eines einzelnen Paketes
                         TRACE_FUNCTION;

include/mw/nl/IEEE_802_15_4_NL.h
@@ -121,6 +121,7 @@ namespace famouso {
                     /*! \brief  Publishes messages that are within the available payload of the driver used.
                      *
                      *  \param[in] p  packet to be published
+                     *  \todo   transmit p.fragment
                      */
                     void deliver(const Packet_t& p) {
                         TRACE_FUNCTION;
@@ -138,18 +139,11 @@ namespace famouso {
                         driver.send(buffer.data, buffer.size);
                     }
 
-                    /*! \brief  Publishes a fragment of a message that exceeds the available payload.
-                     *
-                     *  \param[in] p  packet of the fragment to be published
-                     */
-                    void deliver_fragment(const Packet_t& p) {
-                        TRACE_FUNCTION;
-                    }
-
                     /*! \brief  Transfers the data of the last received message into a packet
                      *          for further use.
                      *
                      *  \param[out] p   packet that is going to contain the received data
+                     *  \todo   transmit p.fragment
                      */
                     void fetch(Packet_t& p) {
                         TRACE_FUNCTION;

include/mw/nl/Packet.h
@@ -48,14 +48,15 @@ struct Packet {
     T snn; // short network name representation
     uint8_t* data; // pointer to data
     uint16_t data_length; // size of data
+    bool fragment;          ///< True if this packet is a fragment (not a complete event)
 
-    Packet() : data(0), data_length(0) {};
+    Packet() : data(0), data_length(0), fragment(false) {};
 
     Packet(const Packet<T>& p)
-            : snn(p.snn), data(p.data), data_length(p.data_length) {}
+            : snn(p.snn), data(p.data), data_length(p.data_length), fragment(p.fragment) {}
 
-    Packet(T address, uint8_t *d, uint16_t length)
-            : snn(address), data(d), data_length(length) {}
+    Packet(T address, uint8_t *d, uint16_t length, bool fragment = false)
+            : snn(address), data(d), data_length(length), fragment(fragment) {}
 };
 
 

commit c2d9e80c9734c2a6f56760c905f9e794fc041dcc

fix broken gateway functionality introduced during AFP integration
In the r1606 patch set, the NetworkAdapter functionality changed in a
way that gateways could not distinguish between local and remote
subscriptions. The gateway assumed always local subscriptions and avoids
constructing forward channels for remote subscriptions. This patch
corrects the introduced wrong behaviour, thus gateways are now able to
determine the origin of subscroptions.

git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@1657 235a714a-1e17-0410-b3bb-947d69a96898

include/mw/nal/NetworkAdapter.h
@@ -192,11 +192,11 @@ namespace famouso {
                      *  \param[in]  bnl the sub network-ID from where the request came.
                      */
                     void event_process_request(famouso::mw::nl::DistinctNL * const bnl) {
-                        if (_ANL_A.id() == bnl)
-                            return _ANL_A.event_process_request(bnl);
-                        else
-                            return _ANL_B.event_process_request(bnl);
                         _bnl = bnl;
+                        if (_ANL_A.id() == bnl)
+                            _ANL_A.event_process_request(bnl);
+                        else
+                            _ANL_B.event_process_request(bnl);
                     }
 
                     /*! \brief Is called by the higher layer to signalise that
@@ -204,9 +204,9 @@ namespace famouso {
                      */
                     void event_processed() {
                         if (_ANL_A.id() == _bnl)
-                            return _ANL_A.event_processed();
+                            _ANL_A.event_processed();
                         else
-                            return _ANL_B.event_processed();
+                            _ANL_B.event_processed();
                         _bnl = 0;
                     }
             };