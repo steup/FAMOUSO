0f7da882eacd95b4ac88802c780b47ba7bc01c11
pwerner <pwerner@235a714a-1e17-0410-b3bb-947d69a96898>
2010-11-29 16:48:03 +0000

    Extends provisonal Time class
    - time relative to first time query in usec
    - operator for outputting a time to the logging framework
    
    git-svn-id: https://svn-eos.cs.ovgu.de/repos/staff/mschulze/Research/MIKRO/famouso@1871 235a714a-1e17-0410-b3bb-947d69a96898

--- include/mw/afp/shared/Time.h
+++ include/mw/afp/shared/Time.h
@@ -75,6 +75,14 @@ class Time {
             return *this;
         }
 
+        uint64_t get_usec() const {
+            return 0;
+        }
+
+        uint64_t get_sec() const {
+            return t;
+        }
+
         bool operator<(const Time & t2) const {
             return t < t2.t;
         }
@@ -84,6 +92,8 @@ class Time {
 // General purpose OS version
 
 #include "boost/thread/xtime.hpp"
+
+#if 0
 
 /*!
  * \brief Represents a time
@@ -107,12 +117,105 @@ class Time {
             return *this;
         }
 
+        Time & add_usec(unsigned int us) {
+            time.nsec += us * 1000;
+            return *this;
+        }
+
+        uint64_t get_usec() const {
+            return time.sec * 1000000 + time.nsec / 1000;
+        }
+
+        uint64_t get_sec() const {
+            return time.sec;
+        }
+
         bool operator<(const Time & t2) const {
             return boost::xtime_cmp(time, t2.time) < 0;
+        }
+
+#else
+
+/*!
+ * \brief Represents a time
+ */
+class Time {
+
+        // usec since first current() call
+        uint64_t time;
+
+        uint64_t get_rep(boost::xtime & t) {
+            return t.sec * 1000000 + t.nsec / 1000;
+        }
+
+    public:
+
+        static void get_current_time(Time & t) {
+            static uint64_t first = 0;
+            boost::xtime tmp;
+            boost::xtime_get(&tmp, boost::TIME_UTC);
+            if (!first) {
+                first = t.get_rep(tmp);
+                t.time = 0;
+            } else {
+                t.time = t.get_rep(tmp) - first;
+            }
+        }
+
+        static Time current() {
+            Time t;
+            get_current_time(t);
+            return t;
+        }
+
+        Time & add_sec(unsigned int s) {
+            time += s * 1000000;
+            return *this;
+        }
+
+        Time & add_usec(unsigned int us) {
+            time += us;
+            return *this;
+        }
+
+        uint64_t get_usec() const {
+            return time;
+        }
+
+        uint64_t get_sec() const {
+            return time / 1000000;
+        }
+
+        bool operator<(const Time & t2) const {
+            return time < t2.time;
+        }
+
+        // Get representation for binary transmission
+        uint64_t get() {
+            return time;
+        }
+
+        // Set from representation for binary transmission
+        void set(uint64_t t) {
+            time = t;
         }
 };
 
 #endif
+
+#endif
+
+/*! \brief Operator to print a Time.
+ *
+ * \param out The output stream to print to.
+ * \param t The Time to print.
+ * \return The output stream.
+ */
+inline ::logging::loggingReturnType &operator <<(::logging::loggingReturnType & out, const Time & t) {
+    uint64_t us = t.get_usec();
+    out << '(' << ::logging::log::dec << us << " us)";
+    return out;
+}
 
 #endif // __TIME_H_2DD6595E115022__
 
